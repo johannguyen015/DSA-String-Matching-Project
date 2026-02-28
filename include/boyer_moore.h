11cc8c00976faf0f55a1d54fde2a743da71ea092
Nguyễn Bá Duy <duynguyen.johan@gmail.com>
2026-02-28 20:52:32 +0700

    Add Boyer-Moore (#4)
    Co-authored-by: TrinhBaoNgoc <trinhtranhuongmai07@gmail.com>

--- include/boyer_moore.h
+++ include/boyer_moore.h
@@ -1,6 +1,8 @@
 #ifndef BOYER_MOORE_H
 #define BOYER_MOORE_H 
 
+#include <vector>
+#include <functional>
 #include "search_strategy.h"
 #include "utilities.h"
 
@@ -11,6 +13,10 @@ public:
     Output locate_keywords(const Input &data) override;
     //returns the name of the algorithm
     std::string get_name() override;
+private: 
+    void build_bad_character(const std::string &pattern, int *badChar); 
+    void build_good_suffix(const std::string &pattern, std::vector <int> &shift);
+    std::vector <int> match_string(int lengthT, int lengthP, const std::function <char(int)> &get_char, const std::string &pattern, const std::vector <int> &shift, int *badChar, long long &comparisons);
 };
 
 #endif
